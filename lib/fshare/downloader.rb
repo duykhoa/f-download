9407451d42077e633ae1328525b22d80f0f9d217
Duy Khoa <duykhoa12t@gmail.com>
2016-05-10 00:56:40 +0800

    Use wget

--- lib/fshare/downloader.rb
+++ lib/fshare/downloader.rb
@@ -9,29 +9,7 @@ module Fshare
     end
 
     def download
-      uri = URI(@url)
-
-      http_object = Net::HTTP.new(uri.host, uri.port)
-      http_object.use_ssl = true if uri.scheme == 'https'
-
-      http_object.start do |http|
-        size = http.request_head(@url)['content-length'].to_i
-        pbar = ProgressBar.create(
-          title: base_name,
-          total: size,
-          format: "%a %B %p%% %t Total #{size/(1024**2)} MB"
-        )
-        request = Net::HTTP::Get.new uri
-
-        http.request request do |response|
-          File.open base_name, 'w' do |io|
-            response.read_body do |chunk|
-              io.write chunk
-              pbar.progress += chunk.size
-            end
-          end
-        end
-      end
+      `wget #{@url} -P #{DOWNLOAD_DIR}`
     end
 
     def base_name
